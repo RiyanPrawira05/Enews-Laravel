66d9907cf4ab883b25d302fb8f714e1d7637daf0
Mochammad Rafi <rafinyadi@gmail.com>
2018-11-10 20:19:28 +0700

    Migrasi

---
+++ database/migrations/2018_11_10_131327_create_berita_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBeritaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('judul');
+            $table->text('header')->nullable()->comment('Gambar Berita');
+            $table->text('isi');
+            $table->integer('user_id')->comment('Kolom Dibuat Oleh');
+            $table->integer('kategori_id')->comment('Kolom Kategori Berita\nOtomatis terisi oleh User id yang login');
+            $table->enum('status', ['0','1'])->comment('Status: 0 Draft, 1 Publish');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('berita');
+    }
+}
