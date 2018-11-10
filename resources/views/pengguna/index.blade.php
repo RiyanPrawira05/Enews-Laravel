7f5a3b23e2e45ca208724ce8f57720d9183f4dda
riyanprawira <riyanprawira05@gmail.com>
2018-11-11 01:40:55 +0700

    Perubahan tabel users)

--- resources/views/pengguna/index.blade.php
+++ resources/views/pengguna/index.blade.php
@@ -12,16 +12,20 @@
                         <table class="table table-striped">
                             <thead>
                                 <th>#</th>
+                                <th>Created At</th>
                                 <th>Nama</th>
                                 <th>Email</th>
+                                <th>Update At</th>
                                 <th><span class="">Actions</span></th>
                             </thead>
                             <tbody>
                                 @foreach ($data as $user)
                                 <tr>
                                     <td></td>
+                                    <td>{{ $user->created_at }}</td>
                                     <td>{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
+                                    <td>{{ $user->updated_at }}</td>
                                     <td>
                                         <a href="{{ route('pengguna.edit', $user->id) }}" class="btn btn-xs btn-warning">Edit</a>
                                         <a href="{{ route('pengguna.destroy', $user->id) }}" class="btn btn-xs btn-danger">Delete</a>
